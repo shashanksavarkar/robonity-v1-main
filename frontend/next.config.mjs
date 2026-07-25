ff1937d8a9590f36d36289ff1b5eee1b1e8cfa5f
Shashank Savarkar <136309519+shashanksavarkar@users.noreply.github.com>
2026-07-26 01:24:42 +0530

    Add redirects configuration to next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "robonity-v1-main.vercel.app",
+          },
+        ],
+        destination: "https://robonity-gsv.vercel.app/:path*",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
